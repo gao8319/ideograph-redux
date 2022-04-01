a04411bc3e9519893f8a7a7e26aa9dae17725ff9
li3zhen1 <master_lee@live.com>
2022-04-01 22:38:14 +0800

    virtualization

--- src/components/PatternSolutionDiagram/PatternSolutionDiagram.tsx
+++ src/components/PatternSolutionDiagram/PatternSolutionDiagram.tsx
@@ -5,9 +5,17 @@ import { edgesSelectors } from "../../store/slice/edgeSlicer"
 import { nodesSelectors } from "../../store/slice/nodeSlicer"
 import { SolutionDiagramCore } from "./SolutionDiagramCore"
 import './SolutionDiagram.css'
+import AutoSizer from "react-virtualized-auto-sizer";
+
+
+import { FixedSizeList as List } from 'react-window'
+import _ from "lodash"
+import React from "react"
+import { Divider } from "@mui/material"
 
 interface ISolutionDiagramProps {
-    onPaint: (svg: SVGSVGElement) => (() => void)
+    onPaint: (svg: SVGSVGElement) => (() => void),
+    style: React.CSSProperties;
 }
 
 interface ISolutionDiagramGridViewProps {
@@ -21,11 +29,11 @@ const SolutionDiagram = (
 ) => {
     const svgRef = useRef<SVGSVGElement>(null);
     useEffect(() => {
-        if(svgRef.current) {
+        if (svgRef.current) {
             return props.onPaint(svgRef.current);
         }
     }, [props.onPaint, svgRef])
-    return <svg ref={svgRef} width={320} height={240} style={{backgroundColor: '#f8f9fa' }}/>
+    return <svg ref={svgRef} width={320} height={240} style={{ backgroundColor: '#f8f9fa' }} />
 }
 
 export const SolutionDiagramGridView = (
@@ -40,20 +48,44 @@ export const SolutionDiagramGridView = (
         edges,
     ), [props.solutions])
 
-    return <div style={{
-        display: 'grid',
-        gridTemplateColumns: `repeat(${props.columnCount},${320}px)`,
-        rowGap: 16,
-        columnGap: 16,
-        padding: 16,
-        paddingTop: 0,
-    }}>
-        {
-            props.solutions.map(
-                sol => <SolutionDiagram onPaint={svg => {
-                    return core.paintOn(svg, sol)
-                }} />
-            )
-        }
-    </div>
+    const chunckedSolutions = React.useMemo(
+        () => _.chunk(props.solutions, 4), [
+        props.solutions
+    ])
+
+    // return <div style={{
+    //     display: 'grid',
+    //     gridTemplateColumns: `repeat(${props.columnCount},${320}px)`,
+    //     rowGap: 16,
+    //     columnGap: 16,
+    //     padding: 16,
+    //     paddingTop: 0,
+    // }}>
+    //     {
+    //         props.solutions.map(
+    //             sol => <SolutionDiagram onPaint={svg => {
+    //                 return core.paintOn(svg, sol)
+    //             }} />
+    //         )
+    //     }
+    // </div>
+
+    return <AutoSizer>
+        {({ height, width }) => (<List itemSize={256}
+            itemCount={chunckedSolutions.length}
+            height={height}
+            width={width}>
+            {
+                (item) => <div style={{ display: 'grid', gridTemplateColumns: `repeat(${props.columnCount}, 320px)`, paddingLeft: 16, paddingRight: 16, columnGap: 16, ...item.style }}>
+                    {
+                        chunckedSolutions[item.index].map(
+                            it => <SolutionDiagram style={item.style} onPaint={svg => {
+                                return core.paintOn(svg, it)
+                            }} />
+                        )
+                    }
+                </div>
+            }
+        </List>)}
+    </AutoSizer>
 }
